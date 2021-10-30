dbbf313679a936b5350817151ca15e7b941cf4e0
Jennifer Shi <shiyating1998@gmail.com>
2021-10-30 10:20:21 +0800

    first commit

---
+++ Client Side/main.js
@@ -0,0 +1,12 @@
+console.log('my name is Jenn');
+
+const fruits = ['appke', 'banana']
+
+fruits[0] ='apple'
+fruits.push('mangos')
+
+fruits.unshift("strawberries")
+console.log(fruits)
+
+
+//JSON.stringify(todo)
